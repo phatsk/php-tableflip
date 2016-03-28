525c8cc30444fec59c28711d32ca3e038891268b
Zach Owen <phatsk@gmail.com>
2016-03-28 13:12:30 -0400

    WordPress plugin file

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: PHP TableFlip for WordPress
+ * Plugin URI: https://github.com/phatsk/php-tableflip
+ * Description: (╯°□°）╯︵ ┻━┻) A simple and stupid Exception/error handler for PHP.
+ * Version: 0.1.3
+ * Author: phatsk
+ * Author URI: https://github.com/phatsk
+ * Text Domain: php-tableflip
+ * Domain Path: /languages
+ */
+
+require_once( dirname( __FILE__ ) . '/lib/flip.php' );
